d7e80aedd1f61e8d87fd1a49a0d0e1f5bd349d56
undugy <undugy@naver.com>
2025-07-25 14:48:22 +0900

    프로젝트 세팅

---
+++ Server/ServerApp/ServerApp.cpp
@@ -0,0 +1,17 @@
+﻿// ServerApp.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include <iostream>
+#include "pch.h"
+class A
+{
+public:
+    int a = 0;
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+    auto t = ObjectPool<A>::GetInstance().Acquire(1);
+
+    int a;
+    std::cin >> a;
+}
