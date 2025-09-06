f6b4fe0d0939dfbbf1690252b59a9fc78df03d99
undugy <70964651+undugy@users.noreply.github.com>
2025-09-07 00:22:47 +0900

    서비스 추가

---
+++ Server/ServerCore/Service.cpp
@@ -0,0 +1,84 @@
+#include "pch.h"
+#include "Service.h"
+#include "IocpContext.h"
+#include "IocpHandler.h"
+#include "NetworkSession.h"
+Service::Service(ServiceType type, SocketAddress address, std::shared_ptr<IocpHandler> iocpHandler, std::function<std::shared_ptr<class NetworkSession>(void)> consturctor, int32_t maxSessionCount)
+	: mServiceType(type), mSockAddress(address), mIocpHandler(iocpHandler), mSessionConstructor(consturctor), mMaxSessionCount(maxSessionCount)
+{
+}
+
+Service::~Service()
+{
+#ifdef VERSION_RIO
+	for (const RIONotifyEvent* event : mRioCQEventList)
+		SocketUtil::RIOEFTable.RIOCloseCompletionQueue(event->rioCQ);
+#endif
+}
+
+std::shared_ptr<NetworkSession> Service::CreateSession()
+{
+	std::shared_ptr<NetworkSession> session = mSessionConstructor();
+	session->SetService(static_pointer_cast<Service>(shared_from_this()));
+
+	if (mIocpHandler->RegisterHandleObject(session->GetHandle()) == false)
+		return nullptr;
+
+	return session;
+}
+
+void Service::AddSession(std::shared_ptr<NetworkSession> session)
+{
+	std::lock_guard	<std::recursive_mutex> guard(mSessionLock);
+	mSessions.insert(session);
+	++mSessionCount;
+}
+
+void Service::ReleaseSession(std::shared_ptr<NetworkSession> session)
+{
+	std::lock_guard	<std::recursive_mutex> guard(mSessionLock);
+	mSessions.erase(session);
+	--mSessionCount;
+}
+
+RIO_CQ& Service::GetRIOCQ(int32_t index)
+{
+	return mRioCQEventList[index % mRioCQEventList.size()]->rioCQ;
+}
+
+void Service::Dispatch(RIORESULT* result, uint32_t numResult)
+{
+
+	int32_t recvCount = 0;
+	int32_t sendCount = 0;
+	int64_t recvSize = 0;
+	int64_t sendSize = 0;
+	for (uint32_t i = 0; i < numResult; i++)
+	{
+		RIOContext* context = reinterpret_cast<RIOContext*>(results[i].RequestContext);
+		std::shared_ptr<IDispatcher> iocpObject = context->mDispatcher;
+		iocpObject->Dispatch(context, results[i].BytesTransferred);
+		switch (context->eventType)
+		{
+		case ContextType::Recv:
+			++recvCount;
+			recvSize += results[i].BytesTransferred;
+			break;
+		case ContextType::Send:
+			++sendCount;
+			sendSize += results[i].BytesTransferred;
+			break;
+		default:
+			break;
+		}
+	}
+	
+}
+
+HANDLE Service::GetHandle()
+{
+	if(mIocpHandler == nullptr)
+		return INVALID_HANDLE_VALUE;
+
+	return mIocpHandler->GetHandle();
+}
---
+++ Server/ServerCore/Service.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "SocketUtil.h"
+#include <functional>
+#include <unordered_set>
+
+enum class ServiceType : uint8_t
+{
+	Listener,
+	Connector
+};
+
+//Session하나에49152/ 동접 5000이면  245, 760, 000
+class IocpHandler;
+class RIONotifyEvent;
+class NetworkSession;
+class Service : public std::enable_shared_from_this<Service>
+{
+protected:
+	ServiceType mServiceType;
+	SocketAddress mSockAddress = {};
+	std::shared_ptr<IocpHandler> mIocpHandler;
+
+	std::unordered_set<std::shared_ptr<NetworkSession>> mSessions;
+	int32_t mSessionCount = 0;
+	int32_t mMaxSessionCount = 0;
+	std::function<std::shared_ptr<class NetworkSession>(void)>mSessionConstructor;
+
+	std::vector<std::shared_ptr<RIONotifyEvent>> mRioCQEventList;
+
+	std::recursive_mutex mSessionLock;
+public:
+	Service(ServiceType type, SocketAddress address, std::shared_ptr<IocpHandler> iocpHandler, std::function<std::shared_ptr<class NetworkSession>(void)> consturctor, int32_t maxSessionCount = 1);
+	virtual ~Service();
+	virtual bool Start() = 0;
+	bool CanStart() const { return mSessionConstructor != nullptr; }
+	virtual void CloseService() {};
+
+	std::shared_ptr<NetworkSession> CreateSession();
+	void AddSession(std::shared_ptr<NetworkSession> session);
+	void ReleaseSession(std::shared_ptr<NetworkSession> session);
+	int32_t GetCurrentSessionCount() { return mSessionCount; }
+	int32_t GetMaxSessionCount() { return mMaxSessionCount; }
+
+	ServiceType GetServiceType() { return mServiceType; }
+	SocketAddress GetSockAddress() { return mSockAddress; }
+	RIO_CQ& GetRIOCQ(int32_t index);
+	virtual void Dispatch(RIORESULT*result, uint32_t numResult);
+
+	std::shared_ptr<IocpHandler> GetIocpHandler() { return mIocpHandler; }
+	HANDLE GetHandle(); 
+};
